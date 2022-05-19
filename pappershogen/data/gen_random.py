bf431e93721e07e63fc4c0280cc74bf74f0cabb8
Matistjati <matistjati@outlook.com>
2022-05-19 12:48:26 +0200

    Prepared test data for pappershogen

--- pappershogen/data/gen_random.py
+++ pappershogen/data/gen_random.py
@@ -16,11 +16,21 @@ def cmdlinearg(name, default=None):
     return default
 
 
-random.seed(int(cmdlinearg('seed')))
-r = int(cmdlinearg('r'))
-c = int(cmdlinearg('c'))
-percent_trees = float(cmdlinearg('percent_trees'))
+def get_manual(id):
+    g = str(id)[0]
+    n = int(str(id)[1])
+    manual_tc = {"1": ["1\nU", "1\nN"],
+                 "2": ["9\nUUUUUUUUN", "8\nNNNUUNUN"],
+                 "3": ["5\nNUUUU", "52\nUUUUUUUUUUNUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU", "172\n" + "U"*172],
+                 "4": ["1\nU", "1\nN", "7\nUNUNUNU", "5\nNUNUN"]}
 
+    return manual_tc[g][n]
+    
+
+def flip(l, m):
+    for i in range(m):
+        l[i] = not l[i]
+    l = (l[:m])[::-1] + l[m:]
 
 def judge_sol(b):
     moves = []
@@ -33,88 +43,10 @@ def judge_sol(b):
         moves.append(len(b))
     return len(moves)
 
-def flip(l, m):
-    for i in range(m):
-        l[i] = not l[i]
-    l = (l[:m])[::-1] + l[m:]
+def to_bin(tc):
+    return [1 if i == 'U' else 0 for i in tc]
 
-def Case1_Preprogrammed(): # 4 st
-    return []
-
-def Case1_Random(): # 24st - Har sett på datan och den ser ok ut
-    cs = []
-    random.seed(28246)
-    for i in range(10): cs.append(Single_Random_Case([3,8], [1,4], 0))
-    random.seed(1987)
-    for i in range(14): cs.append(Single_Random_Case([8,10], [4,5], 0))
-    return cs
-
-def Case2_Preprogrammed(): # 3 st - Har specialfall med 1 och 0 "N"
-    return ["5\nNUUUU", "52\nUUUUUUUUUUNUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU", "172\n" + "U"*172]
-
-def Case2_Random(): # 7 st - Har bara fall där det finns 2 "N"
-    def Case2_Single_Case(n_range):
-        n = random.randint(n_range[0],n_range[1])
-        l = [True for i in range(n)]
-        for j in range(2): l[random.randint(0,n-1)] = False
-        return str(n) + "\n" + "".join("U" if l[i] else "N" for i in range(n))
-    cs = []
-    random.seed(1298)
-    for i in range(2): cs.append(Case2_Single_Case([10,100]))
-    random.seed(8932)
-    for i in range(4): cs.append(Case2_Single_Case([100,200]))
-    cs.append(Case2_Single_Case([200,200]))
-    return cs
-
-def Case3_Preprogrammed(): # 4 st
-    return ["1\nU", "1\nN", "7\nUNUNUNU", "5\nNUNUN"]
-
-def Case3_Random(): # 6 st
-    cs = []
-    def Case3_Single_Case(n_range, bit):
-        n = random.randint(n_range[0],n_range[1])
-        return str(n) + "\n" + "".join(["U" if i%2==bit else "N" for i in range(n)])
-    random.seed(1675)
-    for i in range(2): cs.append(Case3_Single_Case([100,1000], i))
-    for i in range(2): cs.append(Case3_Single_Case([1000,10000], i))
-    for i in range(2): cs.append(Case3_Single_Case([10000,100000], i)) # This contains both ending in "U" and "N" bc that's an important detail
-    return cs
-
-def Case4_Preprogrammed(): # 2st + 4 st + 7 st = 13 st
-    return Sample() + Case1_Preprogrammed() + Case2_Random()
-
-def Case4_Random(): # 7 st
-    cs = []
-    random.seed(63832)
-    for i in range(2): cs.append(Single_Random_Case([10,100], [10,100], 0))
-    for i in range(2): cs.append(Single_Random_Case([100,1000], [100,200], 0)) # Sparse
-    cs.append("123\n" + "".join(["U" if i%2 == 0 else "N" for i in range(123)])) # One from test case 3 for completeness
-    for i in range(2): cs.append(Single_Random_Case([500,1000], [1000,2000], 0)) # So that it gets really scrambled
-    return cs
-
-def Case5_Preprogrammed(): # 2 st + 4 st + 6 st = 12 st
-    return Sample() + Case1_Preprogrammed() + Case3_Random()
-
-def Case5_Random(): # 18 st - Some scaling and then lots of difficult test cases
-    def Case5_Single_Case(n_range, p):
-        n = random.randint(n_range[0],n_range[1])
-        return str(n) + "\n" + "".join(["U" if random.randint(0,100) < p else "N" for i in range(n)])
-    cs = []
-    random.seed(2678)
-    for i in range(2): cs.append(Case5_Single_Case([100,10000],50))
-    for i in range(2): cs.append(Case5_Single_Case([1000,100000],50))
-    for i in range(2): cs.append(Case5_Single_Case([50000,100000],50))
-    cs.append(Case3_Random()[4])
-    cs.append(Case3_Random()[5])
-    random.seed(29784)
-    for i in range(2): cs.append(Case5_Single_Case([50000,100000],10))
-    cs.append(Case5_Single_Case([50000,100000],2))
-    for i in range(2): cs.append(Case5_Single_Case([50000,100000],90))
-    cs.append(Case5_Single_Case([50000,100000],98))
-    for i in range(4): cs.append(Case5_Single_Case([50000,100000],50))
-    return cs
-
-def Single_Random_Case(n_range, m_range, case_seed):
+def Single_Random_Case(n_range, m_range, case_seed=0):
     if case_seed:
         random.seed(case_seed)
     n = random.randint(n_range[0],n_range[1])
@@ -123,38 +55,52 @@ def Single_Random_Case(n_range, m_range, case_seed):
         flip(l,random.randint(1,n))
     return str(n) + "\n" + "".join("U" if l[i] else "N" for i in range(n))
 
-def Case1(): return Sample() + Case1_Preprogrammed() + Case1_Random() # A total of 30 cases just bc
+def Big_n_single_tc(n_range, p):
+    n = random.randint(n_range[0],n_range[1])
+    return str(n) + "\n" + "".join(["U" if random.randint(0,100) < p else "N" for i in range(n)])
 
-def Case2(): return Case2_Preprogrammed() + Case2_Random() # A total of 10 cases to fit with the points.
+def G3_tc(n_range, errors):
+    n = random.randint(n_range[0], n_range[1])
+    c = "U" if random.randint(0,100) < 50 else "N"
+    tc = [c for i in range(n)]
+    for i in range(errors):
+        tc[random.randint(0, len(tc)-1)] = "U" if c == "N" else "N"
+    return str(n) + "\n"+"".join(tc)
 
-def Case3(): return Case3_Preprogrammed() + Case3_Random() # Has the LARGE cases mmm
 
-def Case4(): return Case4_Preprogrammed() + Case4_Random() # Random stuff
+def tc_odd_even(n_range, bit):
+    n = random.randint(n_range[0],n_range[1])
+    return str(n) + "\n" + "".join(["U" if i%2==bit else "N" for i in range(n)])
 
-def Case5(): return Case5_Preprogrammed() + Case5_Random() # 10 BIG ones
 
-print(len(Case1()))
-print(len(Case2()))
-print(len(Case3()))
-print(len(Case4()))
-print(len(Case5()))
-def Case5_Single_Case(n_range, p): # Kopia av den avancerade funtionen
-        n = random.randint(n_range[0],n_range[1])
-        return str(n) + "\n" + "".join(["U" if random.randint(0,100) < p else "N" for i in range(n)])
+random.seed(int(cmdlinearg('seed')))
+n = int(cmdlinearg('n'))
+manual = int(cmdlinearg("mtc", default=0))
+max_moves = int(cmdlinearg("max_moves", default=int(1e9)))
+odd_even = int(cmdlinearg("odd_even", default=0))
+prob = int(cmdlinearg("prob_right", default=-1))
+max_errors = int(cmdlinearg("max_error", default=-1))
 
-def lin(b): # Linjätlösningen för testning
-    moves = []
-    parity = b[0]
-    for pointer in range(len(b) - 1):
-        if b[pointer] != b[pointer + 1]:
-            moves.append(pointer+1)
-            parity = not parity
-    if not parity:
-        moves.append(len(b))
-    return moves
+if manual != 0:
+    print(get_manual(manual))
+else:
+    while True:
+        if n < 2000:
+            tc = Single_Random_Case([1,n], [0,n-1])
+        elif max_errors!=-1:
+            tc = G3_tc([0,n], max_errors)
+        elif odd_even:
+            tc = tc_odd_even([0,n], 1 if odd_even==1 else 0)
+        else:
+            tc = Big_n_single_tc([0,n],prob)
 
-def flip(l, m):
-    for i in range(m):
-        l[i] = not l[i]
-    for i in range(m//2):
-        l[i], l[m-i-1] = l[m-i-1], l[i]
+        if judge_sol(to_bin(tc)) <= max_moves:
+            print(tc)
+            break
+
+
+
+
+
+
+
--- pappershogen/data/generator.sh
+++ pappershogen/data/generator.sh
@@ -19,13 +19,147 @@ sample_manual 2
 # N=1
 group group1 10
 limits max_n=1
-tc_manual manual/g1/g1-manual-1.in
-tc_manual manual/g1/g1-manual-2.in
+tc g1-1 gen_random n=0 mtc=10 seed=0
+tc g1-2 gen_random n=0 mtc=11 seed=0
 
 # N=10 max 5 moves
-group group2
-limits max_n=10
+group group2 20
 include_group sample
 include_group group1
-tc_manual manual/g2/g2-manual-1.in
-tc_manual manual/g2/g2-manual-2.in
+limits max_n=10
+tc g2-1 gen_random  n=0 mtc=20 seed=0
+tc g2-2 gen_random  n=0 mtc=21 seed=0
+tc g2-3 gen_random  n=10 max_moves=5 seed=69
+tc g2-4 gen_random  n=9  max_moves=5 seed=54542745
+tc g2-5 gen_random  n=8  max_moves=5 seed=4586654
+tc g2-6 gen_random  n=7  max_moves=5 seed=876232
+tc g2-7 gen_random  n=6  max_moves=5 seed=7698765
+tc g2-8 gen_random  n=5  max_moves=5 seed=245587
+tc g2-9 gen_random  n=4  max_moves=5 seed=587913413
+tc g2-10 gen_random n=3  max_moves=5 seed=7608734
+tc g2-11 gen_random n=2  max_moves=5 seed=24588946
+tc g2-12 gen_random n=10 max_moves=3 seed=2134654265
+tc g2-13 gen_random n=10 max_moves=3 seed=679746542
+tc g2-14 gen_random n=10 max_moves=3 seed=3412684
+tc g2-15 gen_random n=10 max_moves=3 seed=7846763
+tc g2-16 gen_random n=10 max_moves=3 seed=134243
+tc g2-17 gen_random n=10 max_moves=3 seed=576834
+tc g2-18 gen_random n=10 max_moves=3 seed=13463124
+tc g2-19 gen_random n=10 max_moves=3 seed=23434567675
+tc g2-20 gen_random n=10 max_moves=3 seed=3245423
+tc g2-21 gen_random n=10 max_moves=3 seed=132436
+tc g2-22 gen_random n=10 max_moves=3 seed=3658765
+
+# 2 Wrong
+group group3 10
+include_group group1
+limits max_n=100000
+tc g3-1 gen_random  n=0 mtc=30 seed=0
+tc g3-2 gen_random  n=0 mtc=31 seed=0
+tc g3-3 gen_random  n=0 mtc=32 seed=0
+tc g3-4 gen_random  n=100000 max_error=2 seed=376536
+tc g3-5 gen_random  n=100000 max_error=2 seed=35684687
+tc g3-6 gen_random  n=100000 max_error=2 seed=484673
+tc g3-7 gen_random  n=100000 max_error=2 seed=23453546
+tc g3-8 gen_random  n=100000 max_error=2 seed=35676484
+tc g3-9 gen_random  n=100000 max_error=2 seed=2452376
+tc g3-10 gen_random n=60000  max_error=1 seed=4768476
+tc g3-11 gen_random n=60000  max_error=1 seed=2453657
+tc g3-12 gen_random n=60000  max_error=1 seed=4768354
+tc g3-13 gen_random n=60000  max_error=1 seed=245863
+tc g3-14 gen_random n=60000  max_error=1 seed=36577864
+tc g3-15 gen_random n=90009  max_error=1 seed=24536
+tc g3-16 gen_random n=90009  max_error=1 seed=36784
+tc g3-17 gen_random n=90009  max_error=1 seed=36843
+tc g3-18 gen_random n=90009  max_error=0 seed=3654786
+tc g3-19 gen_random n=90009  max_error=0 seed=3465878
+tc g3-20 gen_random n=90009  max_error=0 seed=35847
+tc g3-21 gen_random n=90009  max_error=0 seed=36345
+tc g3-22 gen_random n=90009  max_error=0 seed=33663
+
+
+# Odd-even
+group group4 10
+include_group group1
+limits max_n=100000
+tc g4-1 gen_random  n=0 mtc=40 seed=0
+tc g4-2 gen_random  n=0 mtc=41 seed=0
+tc g4-3 gen_random  n=0 mtc=42 seed=0
+tc g4-4 gen_random  n=100000 odd_even=1 seed=14331246
+tc g4-5 gen_random  n=100000 odd_even=1 seed=452536
+tc g4-6 gen_random  n=100000 odd_even=1 seed=245653
+tc g4-7 gen_random  n=100000 odd_even=1 seed=35656373675
+tc g4-8 gen_random  n=100000 odd_even=1 seed=32647356
+tc g4-9 gen_random  n=100000 odd_even=1 seed=474476
+tc g4-10 gen_random n=60000  odd_even=1 seed=467886798597
+tc g4-11 gen_random n=60000  odd_even=1 seed=47436
+tc g4-12 gen_random n=60000  odd_even=1 seed=245367
+tc g4-13 gen_random n=60000  odd_even=1 seed=76458795
+tc g4-14 gen_random n=60000  odd_even=2 seed=454678
+tc g4-15 gen_random n=90009  odd_even=2 seed=3654786
+tc g4-16 gen_random n=90009  odd_even=2 seed=476476
+tc g4-17 gen_random n=90009  odd_even=2 seed=24527
+tc g4-18 gen_random n=90009  odd_even=2 seed=25488437
+tc g4-19 gen_random n=90009  odd_even=2 seed=2546385
+tc g4-20 gen_random n=90009  odd_even=2 seed=34876847
+tc g4-21 gen_random n=90009  odd_even=2 seed=4784786
+tc g4-22 gen_random n=90009  odd_even=2 seed=1234245
+
+# n <= 1000
+group group5 20
+include_group sample
+include_group group1
+include_group group2
+limits max_n=1000
+tc g5-1 gen_random  n=1000 seed=35678365
+tc g5-2 gen_random  n=1000 seed=3456245
+tc g5-3 gen_random  n=1000 seed=2453658637585
+tc g5-4 gen_random  n=1000 seed=8579678
+tc g5-5 gen_random  n=1000 seed=2354653
+tc g5-6 gen_random  n=1000 seed=4678467
+tc g5-7 gen_random  n=1000 seed=4678674
+tc g5-8 gen_random  n=1000 seed=2452457
+tc g5-9 gen_random  n=1000 seed=567899587
+tc g5-10 gen_random n=500  seed=68759578
+tc g5-11 gen_random n=500  seed=238792387
+tc g5-12 gen_random n=500  seed=3245673564
+tc g5-13 gen_random n=500  seed=467245
+tc g5-14 gen_random n=696  seed=356753647
+tc g5-15 gen_random n=696  seed=134453
+tc g5-16 gen_random n=696  seed=46735367
+tc g5-17 gen_random n=696  seed=635653
+tc g5-18 gen_random n=696  seed=47684
+tc g5-19 gen_random n=696  seed=36546573
+tc g5-20 gen_random n=696  seed=24554
+tc g5-21 gen_random n=696  seed=746846
+tc g5-22 gen_random n=696  seed=254678
+
+# n <= 10^5
+group group6 30
+include_group sample
+include_group group1
+include_group group2
+include_group group5
+limits max_n=100000
+tc g6-1 gen_random  n=100000 prob_right=100 seed=36577635
+tc g6-2 gen_random  n=100000 prob_right=0   seed=356324
+tc g6-3 gen_random  n=100000 prob_right=10  seed=1345342
+tc g6-4 gen_random  n=100000 prob_right=20  seed=45677649
+tc g6-5 gen_random  n=100000 prob_right=30  seed=1345341
+tc g6-6 gen_random  n=100000 prob_right=40  seed=44684
+tc g6-7 gen_random  n=100000 prob_right=50  seed=356735467
+tc g6-8 gen_random  n=100000 prob_right=60  seed=2456245
+tc g6-9 gen_random  n=100000 prob_right=70  seed=4764786
+tc g6-10 gen_random n=60000  prob_right=80  seed=24536574
+tc g6-11 gen_random n=60000  prob_right=90  seed=356357
+tc g6-12 gen_random n=60000  prob_right=100 seed=43658743
+tc g6-13 gen_random n=60000  prob_right=0   seed=74476
+tc g6-14 gen_random n=60000  prob_right=10  seed=254532
+tc g6-15 gen_random n=90009  prob_right=20  seed=3654768
+tc g6-16 gen_random n=90009  prob_right=30  seed=2453568
+tc g6-17 gen_random n=90009  prob_right=40  seed=2458365
+tc g6-18 gen_random n=90009  prob_right=50  seed=2453657
+tc g6-19 gen_random n=90009  prob_right=60  seed=24578
+tc g6-20 gen_random n=90009  prob_right=70  seed=254794
+tc g6-21 gen_random n=90009  prob_right=80  seed=2458704
+tc g6-22 gen_random n=90009  prob_right=90  seed=807423
